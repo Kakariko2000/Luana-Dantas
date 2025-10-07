fb1e152b9f04f1c01b57490d340c928288ae085e
[dyad] <jeymes.lopes.jl@gmail.com>
2025-10-07 19:02:21 -0300

    [dyad] Removendo a página de contato - wrote 2 file(s), deleted 1 file(s)

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 import HomePage from './components/pages/HomePage';
 import GalleryPage from './components/pages/GalleryPage';
 import AboutPage from './components/pages/AboutPage';
-import ContactPage from './components/pages/ContactPage';
+
 
 const App: React.FC = () => {
   const [currentPage, setCurrentPage] = useState<Page>(Page.Home);
@@ -18,15 +18,13 @@ const App: React.FC = () => {
         return <GalleryPage />;
       case Page.About:
         return <AboutPage />;
-      case Page.Contact:
-        return <ContactPage />;
       default:
         return <HomePage />;
     }
   };
 
   return (
-    <div className="min-h-screen flex flex-col font-comfortaa text-brand-primary"> {/* Alterado de font-sans para font-comfortaa */}
+    <div className="min-h-screen flex flex-col font-comfortaa text-brand-primary">
       <Header currentPage={currentPage} setCurrentPage={setCurrentPage} />
       <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
         {renderPage()}
