84af1a78ea7cf992e9234504e1e851fd9e2e50c0
[dyad] <jeymes.lopes.jl@gmail.com>
2025-10-07 18:53:59 -0300

    [dyad] Atualizando o link do Instagram no rodapé - wrote 1 file(s)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
     <footer className="bg-brand-offwhite py-6 mt-8 border-t border-brand-bg">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center text-brand-secondary">
         <div className="flex justify-center space-x-6 mb-4">
-          <SocialIcon href="https://instagram.com/luanadantasfotografia">
+          <SocialIcon href="https://www.instagram.com/luanadantasfotografia/">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line></svg>
           </SocialIcon>
         </div>
